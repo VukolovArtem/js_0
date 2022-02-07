f429054ea16bc52f3a1dc3f4b2e1a155feeee424
VukolovArtem <vuko.ram55@gmail.com>
2022-02-07 22:14:33 +0200

    Add function CarPrototype fix

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function Car(
   maxSpeed, 
   fuelConsumption, 
   volume)
-  {
-  this.model = model;
-  this.color = color;
-  this.releaseYear = releaseYear;
-  this.speed = speed;
-  this.maxSpeed = maxSpeed;
-  this.fuelConsumption = fuelConsumption;
-  this.volume = volume;
+  { 
+    this.model = model;
+    this.color = color;
+    this.releaseYear = releaseYear;
+    this.speed = speed;
+    this.maxSpeed = maxSpeed;
+    this.fuelConsumption = fuelConsumption;
+    this.volume = volume;
 }
 
 function CarPrototype(){
@@ -26,10 +26,10 @@ function CarPrototype(){
     } else{ return 'MAX'};
   }
   this.stop = function(){
-    if (this.speed>this.maxSpeed){
-      this.speed=this.speed+80;
+    if (this.speed>0){
+      this.speed=this.speed-1;
       return this.speed;
-    } else{ return 'MAX'};
+    } else{ return 'MIN'};
   }
   this.brake = function(){
     return this.speed = 0;
@@ -38,4 +38,6 @@ function CarPrototype(){
 
 Car.prototype = new CarPrototype();
 
-const car1 = new Car('Zaporozhecy', 'red', 1895, 0, 90, 12, 35);
+const car1 = new Car('Zaporozhets', 'red', 1895, 0, 90, 12, 35);
+const car2 = new Car('Mazda', 'black', 2005, 0, 230, 10, 45);
+const car3 = new Car('Tesla', 'black', 2021, 0, 400, 0, 0);
